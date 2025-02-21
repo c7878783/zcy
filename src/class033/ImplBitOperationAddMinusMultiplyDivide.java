7b841af6f5b2f88ab0bc4a7ea382501558551418
和联胜坐馆李家源 <duanshiao@outlook.com>
2025-02-21 19:47:27 +0800

    静态变量MIN写错了，应该是Integer.MIN_VALUE，div中有一行判断应该是b < 0

--- src/class033/ImplBitOperationAddMinusMultiplyDivide.java
+++ src/class033/ImplBitOperationAddMinusMultiplyDivide.java
@@ -1,6 +1,40 @@
 package class033;
 
 public class ImplBitOperationAddMinusMultiplyDivide {
+    public static int MIN = Integer.MIN_VALUE;
+
+    public static int divide(int a, int b){
+        if ((a == MIN) && (b == MIN)){
+            return 1;
+        }
+        if ((a != MIN) && (b != MIN)){
+            return div(a, b);
+        }
+        if (b == MIN){
+            return 0;
+        }
+        if (b == neg(1)){
+            return Integer.MAX_VALUE;
+        }
+        a = add(a, b > 0? b : neg(b));//整数最小是个负数，调用div会无法得到相反数，因为正数最大值要缺一个，所以先削掉一部分，再加回来。
+        int ans = div(a, b);
+        int offset = b > 0? neg(1) : 1;
+        return add(ans, offset);
+    }
+    public static int div(int a, int b){
+        int x = a < 0? neg(a) : a;
+        int y = b < 0? neg(b) : b;
+        int ans = 0;
+
+        for (int i = 30; i >= 0; i = minus(i, 1)){
+            if ((x >> i) >= y) {
+                ans |= (1 << i);
+                x = minus(x, y << i);
+            }
+        }
+
+        return (a < 0) ^ (b < 0)? neg(ans) : ans;
+    }
 
     public static int add(int a, int b){
         int ans = 0;
@@ -29,38 +63,6 @@ public class ImplBitOperationAddMinusMultiplyDivide {
         }
         return ans;
     }
-    public static int MIN = Integer.MAX_VALUE;
 
-    public static int divide(int a, int b){
-        if ((a == MIN) && (b == MIN)){
-            return 1;
-        }
-        if ((a != MIN) && (b != MIN)){
-            return div(a, b);
-        }
-        if (b == MIN){
-            return 0;
-        }
-        if (b == neg(1)){
-            return Integer.MAX_VALUE;
-        }
-        a = add(a, b > 0? b : neg(b));//整数最小是个负数，调用div会无法得到相反数，因为正数最大值要缺一个，所以先削掉一部分，再加回来。
-        int ans = div(a, b);
-        int offset = b > 0? neg(1) : 1;
-        return add(ans, offset);
-    }
-    public static int div(int a, int b){
-        int x = a < 0? neg(a) : a;
-        int y = a < 0? neg(b) : b;
-        int ans = 0;
-        while (x >= y){
-            for (int i = 30; i >= 0; minus(i, 1)){
-                if ((x >> i) >= y) {
-                    ans |= (1 << i);
-                    x = minus(x, y << i);
-                }
-            }
-        }
-        return (a < 0) ^ (b < 0)? neg(ans) : ans;
-    }
+
 }
